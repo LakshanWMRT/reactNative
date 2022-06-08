560926be278e42b73723895538ba0a553de7e375
LakshanWMRT <lakshanwmrt.19@mrt.ac.lk>
2022-06-08 16:53:58 +0530

    added redux

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
  * Changing
  */
 
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import images from './src/config/images';
+// import * as React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import images from './src/config/images';
 
 //  export default function App() {
 //    return (
@@ -13,8 +13,48 @@ import images from './src/config/images';
 //    );
 //  }
 
-import {AppRegistry} from 'react-native';
-import {name as appName} from './app.json';
-import App from './src/main';
 
-AppRegistry.registerComponent(appName, () => App);
+// import {AppRegistry} from 'react-native';
+// import {name as appName} from './app.json';
+// import App from './src/main';
+
+
+
+// AppRegistry.registerComponent(appName, () => App);
+
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+import Root from "./src/main"; 
+export default class reactNative extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Root {...this.props} />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
+
+AppRegistry.registerComponent('reactNative', () => reactNative);
